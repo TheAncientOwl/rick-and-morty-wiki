e63b24af2145f08c566518c0dd41836cd894e79d
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-06-06 20:17:19 +0300

    added content container.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { ThemeProvider } from 'styled-components';
 import Navbar from './components/navbar';
 import theme from './theme';
+import { Container } from './components/ContentElements';
 
 export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Navbar />
-        <div style={{ minHeight: '150vh', color: 'white' }}>
+        <Container>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
           est laborum.
-        </div>
+        </Container>
       </ThemeProvider>
     </>
   );
---
+++ src/components/ContentElements.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  border: 1px solid red;
+  min-height: 80vh;
+  padding: 1rem 2.2rem;
+`;
