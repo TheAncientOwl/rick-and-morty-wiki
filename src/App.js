6c7db3209f641bba5abcb68d18849145203d695b
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-06-12 19:02:27 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { moveToTopAnchor, TopAnchor } from './components/TopAnchor';
 import AppTheme from './constants/AppTheme';
@@ -22,10 +22,6 @@ export default function App() {
   const [page, setPage] = useState(1);
   const [filter, setFilter] = useState('');
   const [data, numberOfPages] = useCharactersData({ page: page, name: filter });
-
-  useEffect(() => {
-    moveToTopAnchor();
-  }, []);
 
   const handleFilterChange = value => {
     setPage(INVALID_PAGE);
